d646099035389f6595610883419c0f4ec0649dc5
Jorge A Ortiz <jorge.ortiz41@upr.edu>
2023-01-05 14:53:10 -0400

    updated delete and update list route functions

--- routes/listroutes.js
+++ routes/listroutes.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const Lists = require("../models/Lists")
 const Listrouter = express.Router()
+const Tasks = require("../models/Tasks")
 
 //get all lists
 Listrouter.get("/lists", async (req, res) => {
@@ -23,6 +24,8 @@ Listrouter.post("/addlist", async (req, res) => {
 
 //update list by id
 Listrouter.put("/updatelist/:id", async (req, res) => {
+    const oldList = await Lists.findById(req.params.id)
+    const listName = oldList.name
     const list = await Lists.findByIdAndUpdate
     (req.params
     .id, req
@@ -30,13 +33,35 @@ Listrouter.put("/updatelist/:id", async (req, res) => {
         new: true
     })
     res.send(list)
+
+    //set task.list to the new list name for the tasks with the old list name using map
+    const tasks = await Tasks.find()
+    tasks.map(async (task) => {
+        if (task.list === listName) {
+            task.list = list.name
+            await task.save()
+        }
+    })
 })
 
 //delete list by id
 Listrouter.delete("/eraselist/:id", async (req, res) => {
+    const list = await Lists.findById(req.params.id)
+    const listName = list.name
     await Lists.findByIdAndDelete(req.params.id)
+    
     //send response in json format
     res.send({message: "List deleted successfully"})
+
+    //set task.list to "None" for the tasks with the deleted list using map
+    const tasks = await Tasks.find()
+    tasks.map(async (task) => {
+        if (task.list === listName) {
+            task.list = "None"
+            await task.save()
+        }
+    })
+
 })
 
 module.exports = Listrouter
